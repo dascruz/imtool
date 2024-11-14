e4fa4823c8968f1dad82317bb2a9cbc8d20ea428
xiutian-wang <100475101@alumnos.uc3m.es>
2024-11-14 18:35:23 +0100

    separar funciones

---
+++ imgsoa/info.cpp
@@ -0,0 +1,12 @@
+#include <imgsoa/imagesoa.hpp>
+#include <iostream>
+
+
+namespace imagesoa {
+  void Image::displayMetadata() const {
+    std::cout << "Image Metadata:\n"
+              << "Width: " << getWidth() << "\n"
+              << "Height: " << getHeight() << "\n"
+              << "Max Color Value: " << getMaxColorValue() << "\n";
+  }
+}
